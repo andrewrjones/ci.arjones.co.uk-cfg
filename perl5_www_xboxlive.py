f53230389db97c078302797c230292ebefa5a94e
andrewrjones <andrewjones86@googlemail.com>
2011-12-30 12:58:35 +0000

    add perl5-WWW-XBoxLive

---
+++ perl5_www_xboxlive.py
@@ -0,0 +1,43 @@
+perl5_www_xboxlive = {}
+
+####### SCHEDULERS
+
+# Configure the Schedulers, which decide how to react to incoming changes.  In this
+
+from buildbot.schedulers.basic import SingleBranchScheduler
+from buildbot.changes import filter
+
+perl5_www_xboxlive['schedulers'] = []
+perl5_www_xboxlive['schedulers'].append(SingleBranchScheduler(
+                            name="perl5-WWW-XBoxLive-commit",
+                            change_filter=filter.ChangeFilter(project='perl5-WWW-XBoxLive'),
+                            treeStableTimer=60,
+                            builderNames=["perl5-WWW-XBoxLive"]))
+
+####### BUILDERS
+
+# The 'builders' list defines the Builders, which tell Buildbot how to perform a build:
+# what steps, and which slaves can execute them.  Note that any particular build will
+# only take place on one slave.
+
+from buildbot.process.factory import BuildFactory
+from buildbot.steps.source import Git
+from buildbot.steps.shell import ShellCommand
+from buildbot.config import BuilderConfig
+
+from modules.dzil_steps.dzil import DzilSmoke
+
+factory_dzil = BuildFactory()
+# check out the source
+factory_dzil.addStep(Git(repourl='git://github.com/andrewrjones/perl5-WWW-XBoxLive.git', mode='copy'))
+# update deps
+factory_dzil.addStep(ShellCommand(command="dzil authordeps | cpanm"))
+factory_dzil.addStep(ShellCommand(command="dzil listdeps | cpanm"))
+# run the tests
+factory_dzil.addStep(DzilSmoke())
+
+perl5_www_xboxlive['builders'] = []
+perl5_www_xboxlive['builders'].append(
+    BuilderConfig(name="perl5-WWW-XBoxLive",
+      slavenames=["mac"],
+      factory=factory_dzil))
